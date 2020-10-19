30a6e6861ede3d5bcc8b1cb791c846d578958b06
thenerdie <stirmanjr@gmail.coom>
2020-10-18 23:31:30 -0400

    Render hit graph

--- ReplicatedStorage/Menus/ResultsMenu.lua
+++ ReplicatedStorage/Menus/ResultsMenu.lua
@@ -6,6 +6,15 @@ local SPUtil = require(game.ReplicatedStorage.Shared.SPUtil)
 
 local ResultsMenu = {}
 
+ResultsMenu.HitColor = {
+	[0] = Color3.fromRGB(255, 0, 0);
+	[1] = Color3.fromRGB(190, 10, 240);
+	[2] = Color3.fromRGB(56, 10, 240);
+	[3] = Color3.fromRGB(7, 232, 74);
+	[4] = Color3.fromRGB(252, 244, 5);
+	[5] = Color3.fromRGB(255, 255, 255);
+}
+
 function ResultsMenu:new(_local_services, _score_data)
 	local self = MenuBase:new()
 	local _results_menu_ui
@@ -27,6 +36,9 @@ function ResultsMenu:new(_local_services, _score_data)
 	function self:cons()
 		_results_menu_ui = EnvironmentSetup:get_menu_protos_folder().ResultsMenuUI:Clone()
 
+		local _song_length_ms = SongDatabase:get_song_length_for_key(_score_data.mapid)+2000
+
+
 		local section_container = _results_menu_ui.SectionContainer
 		local tab_container = _results_menu_ui.TabContainer
 
@@ -40,7 +52,7 @@ function ResultsMenu:new(_local_services, _score_data)
 		local img = ""
 		for i = 1, #_accuracy_marks do
 			local accuracyGrade = _accuracy_marks[i]
-			if _score_data.accuracy*100 >= accuracyGrade then
+			if _score_data.accuracy >= accuracyGrade then
 				img = _grade_images[i]
 				break
 			else
@@ -49,20 +61,27 @@ function ResultsMenu:new(_local_services, _score_data)
 		end
 
 		section_container.Banner.GradeContainer.Grade.Image = img or ""
-		section_container.DataContainer.Accuracy.Data.Text = string.format("%0.2f%%", _score_data.accuracy*100)
+		section_container.DataContainer.Accuracy.Data.Text = string.format("%0.2f%%", _score_data.accuracy)
 
 		--HANDLE SPREAD RENDERING
 		local _spread_display = section_container.SpreadContainer.SpreadDisplay
 
 		local total_judges = #_key_data.HitObjects
+
+		_spread_display.Marvelous.Total.Size = UDim2.new(_score_data.marvelouses/total_judges,0,1,0)
+		_spread_display.Marvelous.TotalNumber.Text = _score_data.marvelouses
+
 		_spread_display.Perfect.Total.Size = UDim2.new(_score_data.perfects/total_judges,0,1,0)
 		_spread_display.Perfect.TotalNumber.Text = _score_data.perfects
 
 		_spread_display.Great.Total.Size = UDim2.new(_score_data.greats/total_judges,0,1,0)
 		_spread_display.Great.TotalNumber.Text = _score_data.greats
 
-		_spread_display.Bad.Total.Size = UDim2.new(_score_data.okays/total_judges,0,1,0)
-		_spread_display.Bad.TotalNumber.Text = _score_data.okays
+		_spread_display.Good.Total.Size = UDim2.new(_score_data.goods/total_judges,0,1,0)
+		_spread_display.Good.TotalNumber.Text = _score_data.goods
+
+		_spread_display.Bad.Total.Size = UDim2.new(_score_data.bads/total_judges,0,1,0)
+		_spread_display.Bad.TotalNumber.Text = _score_data.bads
 
 		_spread_display.Miss.Total.Size = UDim2.new(_score_data.misses/total_judges,0,1,0)
 		_spread_display.Miss.TotalNumber.Text = _score_data.misses
@@ -77,6 +96,16 @@ function ResultsMenu:new(_local_services, _score_data)
 			SongDatabase:get_artist_for_key(_song_key),
 			SongDatabase:get_difficulty_for_key(_song_key)
 		)
+
+		for _, hit_data in pairs(_score_data.hitdeviance) do
+			print(hit_data.note_result)
+			local dot = Instance.new("Frame")
+			dot.BorderSizePixel = 0
+			dot.BackgroundColor3 = ResultsMenu.HitColor[hit_data.note_result]
+			dot.Size = UDim2.new(0,3.5,0,3.5)
+			dot.Position = UDim2.new(hit_data.hit_time_ms/_song_length_ms,0,((hit_data.hit_time_ms-hit_data.expected_hit_time_ms)/240)+0.5,0)
+			dot.Parent = section_container.HitContainer.Hits
+		end
 	end
 
 	function self:get_formatted_data(data)
