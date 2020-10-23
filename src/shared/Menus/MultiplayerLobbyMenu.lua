a7d5a8a7d9261447eca20afb03bec0a2fd8d9e4e
thenerdie <stirmanjr@gmail.coom>
2020-10-22 23:31:51 -0400

    Handle proto, create room on button click

--- src/shared/Menus/MultiplayerLobbyMenu.lua
+++ src/shared/Menus/MultiplayerLobbyMenu.lua
@@ -1,34 +1,37 @@
+local SPUtil = require(game.ReplicatedStorage.Shared.SPUtil)
 local MenuBase = require(game.ReplicatedStorage.Menus.System.MenuBase)
 local EnvironmentSetup = require(game.ReplicatedStorage.RobeatsGameCore.EnvironmentSetup)
+
+local Network = require(game.ReplicatedStorage.Network)
 
 local MultiplayerLobbyMenu = {}
 
 function MultiplayerLobbyMenu:new(_local_services, _header_text, _sub_text, _callback)
 	local self = MenuBase:new()
 	
-	local _multiplayer_lobby_ui
+    local _multiplayer_lobby_ui
+    local _multiplayer_slot_proto
 	
 	function self:cons()
-		_multiplayer_lobby_ui = EnvironmentSetup:get_menu_protos_folder().MultiplayerLobbyUI:Clone()
-		_multiplayer_lobby_ui.TitleDisplay.Text = _header_text
-		_multiplayer_lobby_ui.SubDisplay.Text = _sub_text
-		_multiplayer_lobby_ui.OkayButton.Activated:Connect(function()
-			_callback()
-			_local_services._menus:remove_menu(self)
-		end)
-		_multiplayer_lobby_ui.BackButton.Activated:Connect(function()
-			_local_services._menus:remove_menu(self)
-		end)
-	end
-	
-	function self:hide_okay_button()
-		_multiplayer_lobby_ui.OkayButton.Visible = false
-		return self
-	end
-	
-	function self:hide_back_button()
-		_multiplayer_lobby_ui.BackButton.Visible = false
-		return self
+        _multiplayer_lobby_ui = EnvironmentSetup:get_menu_protos_folder().MultiplayerLobbyUI:Clone()
+        
+        local section_container = _multiplayer_lobby_ui.SectionContainer
+        local tab_container = _multiplayer_lobby_ui.TabContainer
+
+        _multiplayer_slot_proto = section_container.MultiSection.MultiList.MultiListElementProto
+        _multiplayer_slot_proto.Parent = nil
+
+        SPUtil:bind_input_fire(tab_container.CreateRoomButton, function()
+            local _room_id = Network.AddRoom:Invoke({
+                name = "big chungus"
+            })
+        end)
+
+        local _rooms = Network.GetRooms:Invoke()
+
+        for i, room in pairs(_rooms) do
+            
+        end
 	end
 	
 	--[[Override--]] function self:do_remove()
