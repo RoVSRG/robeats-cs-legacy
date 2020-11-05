9d72f91fb108d468d67524e1c76b616190a200eb
thenerdie <stirmanjr@gmail.coom>
2020-11-04 22:24:46 -0500

    /shrug

---
+++ src/shared/Menus/Utils/PreviewManager.lua
@@ -0,0 +1,23 @@
+local PreviewManager = {}
+
+function PreviewManager:new()
+    local self = {}
+
+    local current_game = {}
+
+    function self:start_preview(song_key)
+
+    end
+
+    function self:pause_preview()
+
+    end
+
+    function self:set_time(time)
+
+    end
+
+    return self
+end
+
+return PreviewManager
