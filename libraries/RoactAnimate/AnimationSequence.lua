483a68a20ab6d9151cb9cb83f4e647bd80b27f46
thenerdie <stirmanjr@gmail.coom>
2020-11-06 21:26:43 -0500

    Add a ton of libraries

---
+++ libraries/RoactAnimate/AnimationSequence.lua
@@ -0,0 +1,85 @@
+local Signal = require(script.Parent.Signal)
+
+local AnimationSequence = {}
+AnimationSequence.__index = AnimationSequence
+
+--[[
+	Creates a new animation sequence.
+]]
+function AnimationSequence.new(animations, parallel)
+	local self = setmetatable({
+		_animations = animations,
+		_parallel = parallel,
+		AnimationFinished = Signal.new(),
+	}, AnimationSequence)
+
+	return self
+end
+
+--[[
+	Starts the animation sequence. All animations are run asynchronously; this
+	method does not yield!
+
+	If called when the animation sequence is already playing, this will halt
+	the current sequence and start over from the beginning.
+]]
+function AnimationSequence:Start()
+	-- Do all of this asynchronously.
+	spawn(function()
+		-- If this is a parallel animation set, we can run all of the
+		-- animations at the same time.
+		if self._parallel then
+			-- How many animations are currently running
+			local count = #self._animations
+
+			for _, animation in ipairs(self._animations) do
+				local connection
+				connection = animation.AnimationFinished:Connect(function()
+					connection:Disconnect()
+					count = count - 1
+
+					-- If all of the animations are done, fire the AnimationFinished signal.
+					if count <= 0 then
+						self.AnimationFinished:Fire()
+					end
+				end)
+
+				-- Once we've set the finished connection up, start the animation.
+				-- Starting after the connection is set up prevents issues where
+				-- the animations complete instantly (PrepareSteps).
+				animation:Start()
+			end
+		-- If this is a sequential animation set then we need to run things
+		-- one at a time - this is what _startSequential handles. Start at
+		-- the first animation.
+		else
+			self:_startSequential(1)
+		end
+	end)
+end
+
+--[[
+	Starts playing animations sequentially at a given index.
+]]
+function AnimationSequence:_startSequential(index)
+	local animation = self._animations[index]
+
+	local connection
+	connection = animation.AnimationFinished:Connect(function()
+		connection:Disconnect()
+
+		local newIndex = index + 1
+		-- If the index is greater than the length of the table, this was the
+		-- last animation - we're done!
+		if newIndex > #self._animations then
+			self.AnimationFinished:Fire()
+		-- Otherwise, go on to the next animation.
+		else
+			self:_startSequential(newIndex)
+		end
+	end)
+
+	animation:Start()
+end
+
+return AnimationSequence
---
+++ libraries/RoactAnimate/init.lua
@@ -0,0 +1,42 @@
+local makeAnimatedComponent = require(script.makeAnimatedComponent)
+
+local RoactAnimate = {}
+
+-- Create animated variants of all the Roblox classes
+for _, class in ipairs({ "Frame", "ImageLabel", "ImageButton", "TextButton", "TextBox", "TextLabel", "ScrollingFrame" }) do
+	RoactAnimate[class] = makeAnimatedComponent(class)
+end
+
+local Animation = require(script.Animation)
+local AnimationSequence = require(script.AnimationSequence)
+local PrepareStep = require(script.PrepareStep)
+
+RoactAnimate.Value = require(script.AnimatedValue)
+RoactAnimate.makeAnimatedComponent = makeAnimatedComponent
+
+-- Creates an animation for a value.
+function RoactAnimate.Animate(value, tweenInfo, to)
+	return Animation.new(value, tweenInfo, to)
+end
+
+-- Creates an animation from a table of animations.
+-- These animations will be run one-by-one.
+function RoactAnimate.Sequence(animations)
+	return AnimationSequence.new(animations, false)
+end
+
+-- Creates an animation from a table of animations.
+-- These animations will be run all at once.
+function RoactAnimate.Parallel(animations)
+	return AnimationSequence.new(animations, true)
+end
+
+-- Creates a preparation step.
+-- This allows instantaneous resetting of a value prior to animations completing.
+function RoactAnimate.Prepare(value, to)
+	return PrepareStep.new(value, to)
+end
+
+setmetatable(RoactAnimate, { __call = function(_, ...) return RoactAnimate.Animate(...) end; })
+
+return RoactAnimate
