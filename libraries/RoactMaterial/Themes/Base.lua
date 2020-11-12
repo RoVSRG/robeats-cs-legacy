fda914b5a97f8231bb8ce925de2030fc424eaef4
thenerdie <stirmanjr@gmail.coom>
2020-11-11 22:36:54 -0500

    Get rid of RoactMaterial

--- libraries/RoactMaterial/Themes/Base.lua
+++
@@ -1,3 +0,0 @@
-local Base = {}
-
-return Base
--- libraries/RoactMaterial/Utility/ThemeAccessor.lua
+++
@@ -1,9 +0,0 @@
--- TODO: Convert to use Context API - https://roblox.github.io/roact/advanced/context/
-local ThemeAccessor = {}
-
-function ThemeAccessor.Get(object, key, default)
-	local materialTheme = object._context._materialTheme
-	return materialTheme[key] or default
-end
-
-return ThemeAccessor
