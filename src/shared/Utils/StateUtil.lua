d1a3a6c7e40b9615f24bad842652d2dc425a586f
thenerdie <stirmanjr@gmail.coom>
2020-11-11 22:51:05 -0500

    Add bind to change

--- src/shared/Utils/StateUtil.lua
+++ src/shared/Utils/StateUtil.lua
@@ -1,9 +1,17 @@
+local AssertType = require(game.ReplicatedStorage.Shared.Utils.AssertType)
+
 local Llama = require(game.ReplicatedStorage.Libraries.Llama)
 
 local StateUtil = {}
 
 function StateUtil:new(store)
     local self = {}
+
+    local function runIfNotEqual(a, b, _callback)
+        if a ~= b then
+            _callback(a, b)
+        end
+    end
 
     function self:get_state()
         return store:getState()
@@ -14,6 +22,13 @@ function StateUtil:new(store)
             type = actionName
         })
         store:dispatch(_to_dispatch)
+    end
+
+    function self:bind_to_change(_callback)
+        AssertType:is_non_nil(_callback, "Callback must be a function")
+        return store.changed:connect(function(newState, oldState)
+            _callback(newState, oldState, runIfNotEqual)
+        end)
     end
 
     return self
