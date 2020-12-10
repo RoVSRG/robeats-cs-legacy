445281e6b486cf9f1a8dd82a772a5df90ee327dd
thenerdie <stirmanjr@gmail.coom>
2020-12-09 20:17:20 -0500

    Fix Picarida render

--- src/client/Components/Screens/Picardia.story/init.lua
+++ src/client/Components/Screens/Picardia.story/init.lua
@@ -1,12 +1,14 @@
 local Story = require(game.ReplicatedStorage.Shared.Utils.Story)
 local Roact = require(game.ReplicatedStorage.Libraries.Roact)
 
-local PicardiaApp = Story:new(function()
+local PicardiaApp = Story:new()
+
+function PicardiaApp:render()
     return Roact.createElement("ImageLabel", {
         BackgroundColor3 = Color3.fromRGB(235, 222, 52);
         Image = "rbxassetid://2944248331";
         Size = UDim2.new(1, 0, 1, 0);
     })
-end)
+end
 
 return PicardiaApp
