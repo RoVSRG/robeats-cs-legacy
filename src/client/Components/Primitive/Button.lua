cda697545e2e95ba446a49d2f44e9f7dabd681ae
thenerdie <stirmanjr@gmail.coom>
2020-12-13 18:56:44 -0500

    Extend button component slightly

--- src/client/Components/Primitive/Button.lua
+++ src/client/Components/Primitive/Button.lua
@@ -2,17 +2,24 @@ local Roact = require(game.ReplicatedStorage.Libraries.Roact)
 local Flipper = require(game.ReplicatedStorage.Libraries.Flipper)
 local RoactFlipper = require(game.ReplicatedStorage.Libraries.RoactFlipper)
 
+local SPUtil = require(game.ReplicatedStorage.Shared.Utils.SPUtil)
+
 local Button = Roact.Component:extend("Button")
+
+local function noop() end
 
 function Button:init()
     self.motor = Flipper.SingleMotor.new(0);
     self.motorBinding = RoactFlipper.getBinding(self.motor)
+
+    self.onActivated = self.props.onActivated or noop
 end
 
 function Button:render()
     return Roact.createElement("TextButton", {
         Size = self.motorBinding:map(function(a)
-            return self.props.Size-UDim2.new(0.1*a, 0, 0.1*a, 0)
+            local sb = self.props.shrinkBy or 0.1
+            return (self.props.Size or UDim2.new(1,0,1,0))-UDim2.new(sb*a, 0, sb*a, 0)
         end);
         Position = self.props.Position;
         AnchorPoint = self.props.AnchorPoint;
@@ -20,8 +27,9 @@ function Button:render()
         Text = self.props.Text;
         TextSize = self.props.TextSize;
         BackgroundColor3 = self.motorBinding:map(function(a)
-            local c = self.props.BackgroundColor or Color3.fromRGB(255, 255, 255);
-            local cTo = Color3.fromRGB((c.R*255)-40, (c.G*255)-40, (c.B*255)-40);
+            local c = self.props.BackgroundColor3 or Color3.fromRGB(255, 255, 255);
+            local db = self.props.darkenBy or 40
+            local cTo = Color3.fromRGB((c.R*255)-db, (c.G*255)-db, (c.B*255)-db);
             return c:Lerp(cTo, a);
         end);
         [Roact.Event.MouseEnter] = function()
@@ -48,11 +56,15 @@ function Button:render()
                 dampingRatio = 2.5;
             }))
         end;
+        [Roact.Event.MouseButton1Click] = self.onActivated;
         AutoButtonColor = false;
+        TextColor3 = self.props.TextColor3;
+        Visible = self.props.Visible;
     }, {
-        Roact.createElement("UICorner", {
+        Corner = Roact.createElement("UICorner", {
             CornerRadius = UDim.new(0,4);
-        })
+        });
+        Cdrn = Roact.createFragment(self.props[Roact.Children]);
     })
 end
 
