c652ce669f475edff8a303e3aa600817913e4242
thenerdie <stirmanjr@gmail.coom>
2020-12-13 21:00:33 -0500

    Refactor gameplay components

--- src/client/Components/Screens/Gameplay/GameplayMain.lua
+++ src/client/Components/Screens/Gameplay/GameplayMain.lua
@@ -1,10 +1,24 @@
 local Roact = require(game.ReplicatedStorage.Libraries.Roact)
+local Flipper = require(game.ReplicatedStorage.Libraries.Flipper)
+local RoactFlipper = require(game.ReplicatedStorage.Libraries.RoactFlipper)
 
-local ScoreOverlay = require(script.Parent.ScoreOverlay)
+local Score = require(script.Parent.Score)
+local Accuracy = require(script.Parent.Accuracy)
+local TimeLeft = require(script.Parent.TimeLeft)
+local SpreadDisplay = require(game.ReplicatedStorage.Client.Components.Screens.Results.SpreadDisplay)
+local Combo = require(script.Parent.Combo)
 
 local SPUtil = require(game.ReplicatedStorage.Shared.Utils.SPUtil)
+local NumberUtil = require(game.ReplicatedStorage.Shared.Utils.NumberUtil)
 
 local GameplayMain = Roact.PureComponent:extend("GameplayMain")
+
+function GameplayMain:didMount()
+    self.motor:setGoal(Flipper.Spring.new(1, {
+        frequency = 3.5;
+        dampingRatio = 2;
+    }))
+end
 
 function GameplayMain:init()
 	self.bound_to_frame = SPUtil:bind_to_frame(function()
@@ -12,10 +26,9 @@ function GameplayMain:init()
 			self.props.history:push("/results")
 		end
 	end)
-end
 
-function GameplayMain:willUnmount()
-
+	self.motor = Flipper.SingleMotor.new(0)
+    self.motorBinding = RoactFlipper.getBinding(self.motor)
 end
 
 function GameplayMain:render()
@@ -27,21 +40,52 @@ function GameplayMain:render()
 		BorderSizePixel = 0,
 		Size = UDim2.new(1, 0, 1, 0),
 	}, {
-        StatsOverlay = Roact.createElement(ScoreOverlay, {
-            marvs = stats.marvelouses;
-            perfs = stats.perfects;
+        Score = Roact.createElement(Score, {
+            Position = self.motorBinding:map(function(a)
+                return UDim2.new(0.98*(2-a),0,0.02,0);
+            end);
+            Size = UDim2.new(0.25,0,0.08,0);
+            score = stats.score
+        });
+        Accuracy = Roact.createElement(Accuracy, {
+            Position = self.motorBinding:map(function(a)
+                return UDim2.new(0.98*(2-a),0,0.112,0);
+            end);
+            Size = UDim2.new(0.25,0,0.04,0);
+            accuracy = stats.accuracy
+        });
+        TimeLeft = Roact.createElement(TimeLeft, {
+            Position = self.motorBinding:map(function(a)
+                return UDim2.new(0.98*(2-a),0,0.9,0);
+            end);
+            Size = UDim2.new(0.2,0,0.1,0);
+            time_left = stats.time_left;
+        });
+        Combo = Roact.createElement(Combo, {
+            Position = self.motorBinding:map(function(a)
+                return UDim2.new(0.5*(2-a),0,0.5,0);
+            end);
+            Size = UDim2.new(1,0,0.2,0);
+            combo = stats.combo;
+        });
+        SpreadDisplay = Roact.createElement(SpreadDisplay, {
+            Position = self.motorBinding:map(function(a)
+                return UDim2.new(0.98*(2-a),0,0.2,0);
+            end);
+            Size = UDim2.new(0.127,0,0.3,0);
+            AnchorPoint = Vector2.new(1,0);
+            marvelouses = stats.marvelouses;
+            perfects = stats.perfects;
             greats = stats.greats;
             goods = stats.goods;
             bads = stats.bads;
             misses = stats.misses;
-            score = stats.score;
-            accuracy = stats.accuracy;
-            time_left = stats.time_left;
-            combo = stats.combo;
         });
 		ExitButton = Roact.createElement("TextButton", {
-			BackgroundColor3 = Color3.fromRGB(255, 53, 53),
-			Position = UDim2.new(0.02, 0, 0.05, 0),
+			BackgroundColor3 = Color3.fromHSV(0, 1, 0.85),
+			Position = self.motorBinding:map(function(a)
+				return UDim2.new(NumberUtil.Lerp(-0.5, 0.02, a), 0, 0.05, 0)
+			end);
 			Size = UDim2.new(0.1, 0, 0.05, 0),
 			Font = Enum.Font.GothamSemibold,
 			Text = "Exit",
--- src/client/Components/Screens/Gameplay/GameplayMain.story.lua
+++ src/client/Components/Screens/Gameplay/GameplayMain.story.lua
@@ -1,24 +1,23 @@
-local GameplayMain = require(script.Parent.GameplayMain)
-
-local SPUtil = require(game.ReplicatedStorage.Shared.Utils.SPUtil)
-
 local Roact = require(game.ReplicatedStorage.Libraries.Roact)
 local Rodux = require(game.ReplicatedStorage.Libraries.Rodux)
 local RoactRodux = require(game.ReplicatedStorage.Libraries.RoactRodux)
 
-return function(target)
-    local _unmount = false
-    local component = RoactRodux.connect(function(state, props)
-        return state
-    end, function(dispatch)
-        return {
-            backOut = function()
-                -- TODO: add back out callback
-            end
-        }
-    end)(GameplayMain)
+local GameplayMain = require(script.Parent.GameplayMain)
 
-    local store = Rodux.Store.new(function(state, action)
+local Story = require(game.ReplicatedStorage.Shared.Utils.Story)
+
+local GameplayMainApp = Story:new()
+
+function GameplayMainApp:render()
+    return Roact.createElement(RoactRodux.StoreProvider, {
+        store = self.store
+    }, {
+        MainComponent = Roact.createElement(self.component)
+    })
+end
+
+function GameplayMainApp:init()
+    self.store = Rodux.Store.new(function(state, action)
         state = state or {
             stats = {
                 marvelouses = 1;
@@ -33,20 +32,12 @@ return function(target)
                 combo = 0;
             }
         }
-
         return state
     end)
 
-    local fr = Roact.createElement(RoactRodux.StoreProvider, {
-        store = store
-    }, {
-        MainComponent = Roact.createElement(component)
-    })
-
-    local tree = Roact.mount(fr, target)
-
-    return function()
-        _unmount = true
-        Roact.unmount(tree)
-    end
+    self.component = RoactRodux.connect(function(state)
+        return state
+    end)(GameplayMain)
 end
+
+return GameplayMainApp
