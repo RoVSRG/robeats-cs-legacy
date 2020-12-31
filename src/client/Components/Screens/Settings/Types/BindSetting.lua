39dcaaae5a12cd68c15c584aa79cd36fcdde3832
thenerdie <stirmanjr@gmail.coom>
2020-12-31 16:04:06 -0500

    :derp:

--- src/client/Components/Screens/Settings/Types/BindSetting.lua
+++ src/client/Components/Screens/Settings/Types/BindSetting.lua
@@ -3,17 +3,12 @@ local Roact = require(game.ReplicatedStorage.Libraries.Roact)
 local BaseSetting = require(script.Parent.BaseSetting)
 
 local Button = require(game.ReplicatedStorage.Client.Components.Primitive.Button)
-local Slider = require(game.ReplicatedStorage.Client.Components.Primitive.Slider)
 
 local BindSetting = Roact.Component:extend("BindSetting")
 
 function BindSetting:init()
     self.changeSetting = self.props.changeSetting
     self.increment = self.props.increment or 1
-
-    self.getDerivedText = self.props.getDerivedText or function(value)
-        return "kimi is GAE"
-    end;
 end
 
 function BindSetting:render()
@@ -22,15 +17,16 @@ function BindSetting:render()
         BorderColor3 = Color3.fromRGB(0, 0, 0),
         BorderSizePixel = 0,
         Size = UDim2.new(1, 0, 1, 0),
-        title = self.props.title;
-        value = self.props.value;
-        getDerivedText = self.getDerivedText;
     }, {
         ChangeButton = Roact.createElement(Button, {
             Position = UDim2.new(0.28, 0, 0.600000024, 0),
             Size = UDim2.new(0.05, 0, 0.349999994, 0),
             AnchorPoint = Vector2.new(0, 0.5),
             shrinkBy = 0.007;
+            darkenBy = 13;
+            BackgroundColor3 = Color3.fromRGB(40, 40, 40);
+            TextColor3 = Color3.fromRGB(255, 255, 255);
+            Text = "Change";
         })
     })
 end
