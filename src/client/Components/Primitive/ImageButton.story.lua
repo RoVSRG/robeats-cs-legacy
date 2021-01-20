d61c56d607daf5ba3ee1566ef8b2b4705d29383c
astralkingdummy <cjansah11@gmail.com>
2021-01-20 17:29:52 -0500

    not fully functional, will fix when i get back from the outdoors or tomorrow (unless regen wants to do it lmfao xd!)

---
+++ src/client/Components/Primitive/ImageButton.story.lua
@@ -0,0 +1,18 @@
+local Roact = require(game.ReplicatedStorage.Libraries.Roact)
+
+local ImageButton = require(script.Parent.ImageButton)
+
+local Story = require(game.ReplicatedStorage.Shared.Utils.Story)
+
+local ImageButtonApp = Story:new()
+
+function ImageButtonApp:render()
+    return Roact.createElement(ImageButton, {
+        Size = UDim2.new(1,0,1,0);
+        Position = UDim2.new(0.5, 0, 0.5, 0);
+        AnchorPoint = Vector2.new(0.5, 0.5);
+        Image = "rbxassetid://2944248331";
+    })
+end
+
+return ImageButtonApp
